bc79adcd43cab22bc966f255ebc13357293f5133
danifaso02 <110115694+danifaso02@users.noreply.github.com>
2022-12-05 10:42:54 +0100

    Update PageTitle.vue

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -1,15 +1,9 @@
 <script>
 export default {
-  data() {
-    return {
-      title: 'Il titolo della pagina'
-    }
-  }
 }
 </script>
 
 <template>
-  <h1>{{ title }}</h1>
 </template>
 
 <style scoped></style>
