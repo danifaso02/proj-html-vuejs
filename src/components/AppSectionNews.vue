181b38bcb1cff3bfb3c899f008800e0d4a674468
danifaso02 <danifaso02@gmail.com>
2022-12-07 18:01:33 +0100

    aggiunta btn

--- src/components/AppSectionNews.vue
+++ src/components/AppSectionNews.vue
@@ -11,7 +11,7 @@ export default {
             <div class="news-image">
                 <img src="../assets/images/blog-46.jpg" alt="">
                 <div class="description">
-                    <span>PHOTOGRAPHY</span>
+                    <button>PHOTOGRAPHY</button>
                     <h3>How To Take Better Concert Pictures in 30 Seconds</h3>
                 </div>
             </div>
@@ -19,7 +19,7 @@ export default {
             <div class="news-image">
                 <img src="../assets/images/blog-47.jpg" alt="">
                 <div class="description">
-                    <span>GADGETS</span>
+                    <button>GADGETS</button>
                     <h3>Gadgets That Make Your Smartphone Even Smarter</h3>
                 </div>
             </div>
@@ -27,7 +27,7 @@ export default {
             <div class="news-image">
                 <img src="../assets/images/blog-48.jpg" alt="">
                 <div class="description">
-                    <span>TRAVEL</span>
+                    <button>TRAVEL</button>
                     <h3>20 Top-Rated Tourist Attractions in Manhattan</h3>
                 </div>
             </div>
@@ -35,7 +35,7 @@ export default {
             <div class="news-image">
                 <img src="../assets/images/blog-49.jpg" alt="">
                 <div class="description">
-                    <span>LIFESTYLE</span>
+                    <button>LIFESTYLE</button>
                     <h3>The Best Way to Ride a Motorcycle</h3>
                 </div>
             </div>
@@ -43,7 +43,7 @@ export default {
             <div class="news-image">
                 <img src="../assets/images/blog-50.jpg" alt="">
                 <div class="description">
-                    <span>TRAVEL</span>
+                    <button>TRAVEL</button>
                     <h3>5 Fun Things to Do at the Beach</h3>
                 </div>
             </div>
@@ -51,7 +51,7 @@ export default {
             <div class="news-image">
                 <img src="../assets/images/blog-51.jpg" alt="">
                 <div class="description">
-                    <span>RECIPES</span>
+                    <button>RECIPES</button>
                     <h3>Amazingly Fresh Fruit And Herb Drinks For Summer</h3>
                 </div>
             </div>
@@ -91,17 +91,16 @@ img{
     width: 100%;
 }
 
-span{
+button{
     background-color: #0088cc;
     padding: 5px;
     border-radius: 5px;
+    border: 1px lightgrey;
+    color: #fff;
 }
 
 h3{
     margin-top: 15px;
-}
-
-span, h3{
     color: #fff;
 }
 
